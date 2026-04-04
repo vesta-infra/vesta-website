4c4d0023502ff4c31ac24ac087b3140957e804c2
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2026-04-04 23:17:45 +0100

    setup pages

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { z, defineCollection } from 'astro:content';
+
+const docs = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    order: z.number(),
+    section: z.string(),
+  }),
+});
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    author: z.string().default('Vesta Team'),
+  }),
+});
+
+export const collections = { docs, blog };
